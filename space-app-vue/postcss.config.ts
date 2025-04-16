403fa09d2bf8d28933e967b54ab5579ae7220dfe
aigerim tuishieva <gerardinearmstrong@gmail.com>
2025-04-17 02:36:04 +0500

    setup configs, dependencies, bootstrapping

---
+++ space-app-vue/postcss.config.ts
@@ -0,0 +1,12 @@
+export default {
+    plugins: {
+        autoprefixer: {},
+        "postcss-custom-properties": {},
+        "postcss-import": {},
+        "postcss-nested": {},
+        /*"@csstools/postcss-global-data": {
+            files: ["./path/to/media-width-variables"],
+        },*/
+        "postcss-custom-media": {},
+    },
+};
